8bbad1e160e7f31e883ef12559bb9fed9ff21d86
Indosaram <freedomzero91@gmail.com>
2025-04-28 14:29:24 +0900

    initial commit

---
+++ target/debug/deps/aws_intro_book-6c7278883f766e91.d
@@ -0,0 +1,5 @@
+/Users/indo/code/Project/aws_intro_book/target/debug/deps/libaws_intro_book-6c7278883f766e91.rmeta: src/main.rs
+
+/Users/indo/code/Project/aws_intro_book/target/debug/deps/aws_intro_book-6c7278883f766e91.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/aws_intro_book-b4bb8a489edeae47.d
@@ -0,0 +1,5 @@
+/Users/indo/code/Project/aws_intro_book/target/debug/deps/libaws_intro_book-b4bb8a489edeae47.rmeta: src/main.rs
+
+/Users/indo/code/Project/aws_intro_book/target/debug/deps/aws_intro_book-b4bb8a489edeae47.d: src/main.rs
+
+src/main.rs:
